bb38b6c646dd8237b4efc1db581ae1f8f7cd6484
SriHarsha Vajapai <harshavajapeyee4433@gmail.com>
2021-09-23 17:51:39 +0530

    Add Special Pairs question

--- Divide and Conquer/Special Pairs/README.md
+++ Divide and Conquer/Special Pairs/README.md
@@ -2,19 +2,24 @@
 Given an array A of integers of N length, elements (A[i], A[j]) becomes a special pair if i < j and A[i] > 2*Aj]. Write an efficient program to find the number of special pairs present in A. If there are no special pairs, return -1.
 
 ### Input
-First line contains a single integer N
+First line contains a single integer N <br/>
 Second line contains N space separated integers A[i]
 
 ### Output
-Print a single integer representing the number of special pairs. If there are no special pairs, print -1.
+Print a single integer representing the number of special pairs. <br/>
+If there are no special pairs, print -1.
 
 ### Constraints
-1 <= N <= 50000
+1 <= N <= 50000 <br/>
 -10^9 <= A[i] <= 10^9
 
 ### Sample Input 1
-`5
-1 3 2 3 1`
+```
+5
+1 3 2 3 1
+```
 
 ### Sample Output 1
-`2`
+```
+2
+```
--- Divide and Conquer/Special Pairs/main.cpp
+++ Divide and Conquer/Special Pairs/main.cpp
@@ -1,10 +1,65 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int count = 0;
+
+void merge(int *arr, int start, int mid, int end)
+{
+    int i = start, j = mid + 1, k = start;
+    int r[end + 1];
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] > 2 * arr[j])
+        {
+            count += (mid + 1 - i);
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    i = start, j = mid + 1;
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+            r[k++] = arr[i++];
+        else
+            r[k++] = arr[j++];
+    }
+    while (i <= mid)
+        r[k++] = arr[i++];
+    while (j <= end)
+        r[k++] = arr[j++];
+    for (int i = start; i <= end; i++)
+    {
+        arr[i] = r[i];
+    }
+}
+
+void merge_sort(int *arr, int start, int end)
+{
+    if (start < end)
+    {
+        int mid = start + (end - start) / 2;
+        merge_sort(arr, start, mid);
+        merge_sort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+}
+
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    merge_sort(arr, 0, n - 1);
+    // for(int i=0;i<n;i++) cout<<arr[i];
+    if (count)
+        cout << count;
+    else
+        cout << -1;
     return 0;
 }
